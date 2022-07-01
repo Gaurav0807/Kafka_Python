eeffb6b66ff1a2249c19e288e2c420a1fcd679ae
Gaurav0807 <gauravrawat141999@gmail.com>
2022-07-02 00:28:50 +0530

    structured_streaming

---
+++ Structured_Streaming/spark4.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+
+
+if __name__=="__main__":
+    spark = SparkSession.builder.appName("spark_streaming").config("spark.sql.shuffle.partitions",3)\
+        .config("spark.streaming.stopGracefullyOnShutdown",True)\
+        .config("spark.sql.streaming.schemaInference",True)\
+        .getOrCreate()
+
+    df = spark.readStream.format("json").option("path","InputFolder").option("maxFilesPerTrigger",1).load()
+
+
+    df.createOrReplaceTempView("Orders")
+
+    df_= spark.sql("select count(*) from Orders where order_status = 'COMPLETE'")
+    
+    ##write to console
+    result = df_.writeStream.format("console").outputMode("complete")\
+        .option("path","myoutputFolder")\
+        .option("checkpointLocation","checkppoint-location5")\
+        .start()
+
+
+    result.awaitTermination()
+
+
---
+++ Structured_Streaming/spark5.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql import functions as F
+
+if __name__=="__main__":
+    spark = SparkSession.builder.appName("spark_streaming").config("spark.sql.shuffle.partitions",3)\
+        .config("spark.streaming.stopGracefullyOnShutdown",True)\
+        .getOrCreate()
+
+    df = spark.readStream.format("socket").option("host","localhost").option("port","9092").load()
+    #df_.printSchema()
+
+    schema = StructType([
+    StructField("order_id",IntegerType()),
+    StructField("order_date",StringType()),
+    StructField("order_customer_id",IntegerType()),
+    StructField("order_status",StringType()),
+    StructField("amount",IntegerType())
+   ])
+
+    valueDf = df.select(from_json(F.col("value"),schema).alias("Value"))
+
+    redefinedOrdersDf = valueDf.select("Value.*")
+
+    #redefinedOrdersDf.printSchema()
+
+    windowAggDF = redefinedOrdersDf.groupBy(window(F.col("order_date"),"15 minute")).agg(F.sum("amount").alias("totalInvoice"))
+
+    resultDF  = windowAggDF.select("window.start","window.end","totalInvoice")
+
+    orderQuery = resultDF.writeStream.format("console").outputMode("update").option("chckpointLocation","chk-location1")\
+        .trigger(processingTime="15 second").start()
+
+    orderQuery.awaitTermination() 
+
+
+
+    
+    
+
+
+
+
